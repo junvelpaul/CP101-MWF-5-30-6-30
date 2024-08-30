ae45039f0e9637870d4fe1f22b49fe0c8a07f52a
junvelpaul <junvelpauldagz@gmail.com>
2024-08-30 20:35:40 +0800

    Create Dagayloan.py

---
+++ Activity 3/Dagayloan.py
@@ -0,0 +1,8 @@
+hours_worked = float(input("35: "))
+hourly_rate = float(input("2.75: "))
+
+# calculate total pay
+total_pay = hours_worked * hourly_rate
+
+# Display the result
+print(f"96.25: {96.25:.2f}")
